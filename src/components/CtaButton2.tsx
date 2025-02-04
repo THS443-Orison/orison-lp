04ea52e5c092bf3821aa4a5997ffa4567e1ef80d
Kusanagi <kiri11280201@gmail.com>
2025-02-04 13:48:35 +0900

    修正

--- src/components/CtaButton2.tsx
+++ src/components/CtaButton2.tsx
@@ -1,8 +1,9 @@
 "use client";
+
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
-import PostBtn from "@/assets/images/postBtn.gif";
 import Kiritori from "@/assets/images/kiritori.png";
+
 export default function CtaButton2() {
   const [isScrolled, setIsScrolled] = useState(false);
 
@@ -21,7 +22,7 @@ export default function CtaButton2() {
   return (
     <div className="hover-container hover:animate-bounce hover:animate-float mt-[-1px]">
       {isScrolled ? (
-        <Image src={PostBtn} alt="second kiritori image" unoptimized />
+        <Image src={'https://glb.orison-ms.jp/postBtn.gif'} alt="second kiritori image" unoptimized />
       ) : (
         <Image src={Kiritori} alt="first kiritori image" />
       )}
